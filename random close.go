dee0da4892908f2e8bf97aa89cec6fd2837818fc
BrerRabbit <aranteerl@gmail.com>
2020-10-04 19:48:04 +0400

    fix smth

--- random close.go
+++ random close.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
-func hello(ch chan string, wg *sync.WaitGroup) {
+func hello(ch chan string) {
 	for {
 		time.Sleep(time.Second)
 		fmt.Println("Привет")
@@ -16,13 +15,12 @@ func hello(ch chan string, wg *sync.WaitGroup) {
 }
 
 func main() {
-	var wg sync.WaitGroup
 	channel := make(chan string)
 	rand.Seed(time.Now().UTC().UnixNano())
-	go hello(channel, &wg)
+	go hello(channel)
 	go func() {
 		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-		wg.Wait()
+		//wg.Wait()
 		close(channel)
 	}()
 
